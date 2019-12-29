c8d69df6a1090577107fac52d41137ae58148f7c
firmin <firminapp@gmail.com>
2019-12-29 13:02:09 +0100

    initial

---
+++ src/tppoo/Phone.java
@@ -0,0 +1,34 @@
+package tppoo;
+
+public class Phone {
+    private String mark;
+    private int price;
+    private int ram;
+    public Phone(){
+
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+}
